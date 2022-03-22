9dbc4bafca18dd8c092b08e403204016a99bc6b4
Ginelle D'souza <64312327+ginelledsouza@users.noreply.github.com>
2022-03-22 23:11:30 +0530

    Updated Script

--- includes/function.py
+++ includes/function.py
@@ -1,10 +1,15 @@
+from airflow.hooks.base_hook import BaseHook
 from airflow.models import Variable
 
-post_config = Variable.get("Access Credential", deserialize_json=True)
+post_config = Variable.get("zelle_columns", deserialize_json=True)
+conn = BaseHook.get_connection('zelle_connection')
 
 # Database links and creds (PostgreSQL)
-user = post_config['user'] 
-password = post_config['password'] 
-host = post_config['host'] 
-port = post_config['port'] 
-database = post_config['database'] 
+user = conn.login
+password = conn.password 
+host = conn.host
+port = conn.port
+database = conn.schema
+
+# Database links and creds (PostgreSQL)
+filtered_col = post_config['data_columns']
